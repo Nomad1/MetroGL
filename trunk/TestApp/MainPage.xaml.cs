739d3ab7ef20d37a3bb195d5ae14dd44d75265b9
Alexei Garbuzenko <gorbuz@gmail.com>
2014-12-27 16:53:26 +0200

    fish example update

--- trunk/TestApp/MainPage.xaml.cs
+++ trunk/TestApp/MainPage.xaml.cs
@@ -15,8 +15,8 @@ namespace TestApp
     {
         private readonly OpenGL m_gl;
         private float m_angle;
-        private uint m_textureId1;
-        private uint m_textureId2;
+        private int m_textureId1;
+        private int m_textureId2;
         private float m_height;
         private float m_width;
         private int m_frames;
@@ -48,39 +48,38 @@ namespace TestApp
             GL.Enable(All.Texture2D);
             GL.Color4(1.0f, 1.0f, 1.0f, 1.0f);
 
-            GL.TexParameter(All.Texture2D, All.TextureCropRectOes, new int[] { 0, 1024, 1024, -1024 });
+            GL.TexParameter(All.Texture2D, All.TextureCropRectOes, new int[] { 0, 0, 1024, 1024 });
             GL.BindTexture(All.Texture2D, m_textureId1);
-            //GL.DrawTex(100, 500, 0, 250, 250);
+            GL.DrawTex(0, - (m_width - m_height) / 2, 0, m_width, m_width);
             
-            for (int i = 0; i < 500; i++)
+            for (int i = 0; i < 10; i++)
             {
-                if (i%10 > 5)
+                if (i % 2 == 0)
                 {
-                    GL.TexParameter(All.Texture2D, All.TextureCropRectOes, new int[] {0, 1024, 1024, -1024});
-                    GL.BindTexture(All.Texture2D, m_textureId1);
+                    GL.BindTexture(All.Texture2D, m_textureId2);
+                    GL.TexParameter(All.Texture2D, All.TextureCropRectOes, new int[] { 0, 0, 256, 256 });
                 }
                 else
                 {
                     GL.BindTexture(All.Texture2D, m_textureId2);
-                    GL.TexParameter(All.Texture2D, All.TextureCropRectOes, new int[] { 0, 512, 512, -512 });
+                    GL.TexParameter(All.Texture2D, All.TextureCropRectOes, new int[] { 256, 0, 256, 256 });
                 }
                 
-                float pos = (float) Math.Sin(i/100.0f + Environment.TickCount/1000.0f);
-                GL.DrawTex(10 + (i % 200) * 5, (pos + 1)* m_height + (i/200) * 10, 0, 50, 50);
+                int aqPadding = 20;
+                int fishSize = 128;
+                int aqWidth = (int)m_width - aqPadding * 2 - fishSize;
+
+                float x = (Environment.TickCount / (i + 10)) % aqWidth;
+                float alpha = 1.0f;
+                if (x < fishSize)
+                    alpha = x / fishSize;
+                else
+                    if (x > aqWidth - fishSize)
+                        alpha = (aqWidth - x - fishSize) / 128.0f;
+
+                GL.Color4(1.0f, 1.0f, 1.0f, alpha);
+                GL.DrawTex(x + aqPadding, m_height / 20 * (i + 5), 0, fishSize, fishSize);
             }
-            /*
-            GL.BindTexture(All.Texture2D, m_textureId2);
-            GL.TexParameter(All.Texture2D, All.TextureCropRectOes, new int[] { 0, 512, 512, -512 });
-            for (int i = 500; i < 1000; i++)
-            {
-                float pos = (float)Math.Sin(i / 100.0f + Environment.TickCount / 1000.0f);
-                GL.DrawTex(10 + (i % 200) * 5, (pos + 1) * m_height + (i / 200) * 10, 0, 50, 50);
-            }
-            /*
-            GL.BindTexture(All.Texture2D, m_textureId2);
-            GL.Color4(1.0f, 1.0f, 1.0f, 1.0f);
-            GL.TexParameter(All.Texture2D, All.TextureCropRectOes, new int[] { 0, 512, 512, -512 });
-            GL.DrawTexfOES(400, 500, 0, 200, 200);*/
 
             m_updateFrames++;
             if (m_updateFrames >= Math.Min(m_updateFps * 2, 60))
@@ -96,8 +95,8 @@ namespace TestApp
         {
             Debug.WriteLine("Init callback");
 
-            m_textureId1 = GL.LoadTexture("ui.png");
-            m_textureId2 = GL.LoadTexture("background.bmp");
+            m_textureId1 = (int)GL.LoadTexture("back.jpg");
+            m_textureId2 = (int)GL.LoadTexture("fish.png");
         }
 
         private void Resize(int width, int height)
@@ -105,7 +104,7 @@ namespace TestApp
             Debug.WriteLine("Resize callback " + width + ", " + height);
             GL.Ortho(0, width, 0, height, -1, 1);
             m_width = width;
-            m_height = (height / 2) - 50;
+            m_height = height;
         }
 
     }
